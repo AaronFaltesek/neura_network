84f6c285c3fad7b8d1dd6fb5afe19a879d7672ef
gitatx <aaron.faltesek@gmail.com>
2018-03-21 21:57:15 -0500

    AKF: adding initial work from video_3

---
+++ neural_network_3/__main__.py
@@ -0,0 +1,13 @@
+import numpy
+
+
+def main():
+    print("Entering main:")
+    x=numpy.add(3,7)
+    print(x)
+    y=numpy.binary_repr(230)
+    print(y)
+    print("Exiting main:")
+
+if __name__ == '__main__':
+    main()
